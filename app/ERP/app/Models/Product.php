d53ea2cd0695bdbeb71664317eb7c7f8ae4614a1
albert199577 <mso729049@gmail.com>
2023-02-09 17:29:43 +0800

    feat: set type and product one tp many associate

--- app/ERP/app/Models/Product.php
+++ app/ERP/app/Models/Product.php
@@ -13,4 +13,9 @@ class Product extends Model
     {
         return $this->belongsTo(Brand::class);
     }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/ERP/database/seeders/ProductsTableSeeder.php
+++ app/ERP/database/seeders/ProductsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Brand;
 use App\Models\Product;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,9 +18,11 @@ class ProductsTableSeeder extends Seeder
     public function run()
     {
         $brands = Brand::all();
-
-        Product::factory()->count(50)->make()->each(function ($product) use ($brands) {
+        $types = Type::all();
+        
+        Product::factory()->count(50)->make()->each(function ($product) use ($brands, $types) {
             $product->brand_id = $brands->random()->id;
+            $product->type_id = $types->random()->id;
             $product->save();
         });
     }
